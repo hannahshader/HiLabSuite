b8de225070b0c950805bd33f54685d50ec850721
hannahshader <38895141+hannahshader@users.noreply.github.com>
2023-06-15 10:02:31 -0400

    Progress for formatting files

--- data_structures/structure_interact.py
+++ data_structures/structure_interact.py
@@ -2,20 +2,28 @@ from typing import Any, Dict, List
 from data_structures.init_utterance_dict import InitUtteranceDict
 from data_structures.marker_utterance_dict import MarkerUtteranceDict
 import copy
+import os
 from pydantic import BaseModel
 
 from collections import OrderedDict
 from typing import OrderedDict as OrderedDictType, TypeVar
 
+## Used to be OUT_PATH = "/Users/yike/Desktop/plugin_output"
+OUT_PATH = "Temporary"
+
 
 # outermost layer, wraps around marker_utterance_obj
 class StructureInteract:
-    ##def __init__(self, data_structure_arg):
-    ##    self.data_structure = data_structure_arg
-    def __init__(self, utt_data=None):
+    def __init__(self, utt_data=None, output_path=None):
         int_utterance_obj = InitUtteranceDict(utt_data)
         marker_utterance_obj = MarkerUtteranceDict(int_utterance_obj)
         self.data_structure = marker_utterance_obj
+
+        if output_path:
+            self.output = output_path
+            os.makedirs(output_path, exist_ok=True)
+        else:
+            self.output = OUT_PATH
 
     def interact_insert_marker(self, item):
         self.data_structure.insert_marker(self.data_structure, item)
@@ -29,6 +37,3 @@ class StructureInteract:
     # Calls apply_insert_marker, which takes an instance of MarkerUtterance and a list of functions
     def apply_markers(self, apply_functions):
         self.data_structure.apply_insert_marker(self.data_structure, apply_functions)
-
-    def is_utt(self, string):
-        return self.data_structure.is_speaker_utt(string)
---
+++ format/csv.py
@@ -0,0 +1,106 @@
+from typing import Dict, Any
+import os
+import csv
+from configs.configs import (
+    INTERNAL_MARKER,
+    load_label,
+    PLUGIN_NAME,
+    OUTPUT_FILE,
+    CSV_FORMATTER,
+) 
+
+
+class CSVPlugin:
+    def __init__(self, structure_interact_instance):
+        self.structure_interact_instance = structure_interact_instance
+
+
+    def apply(self, dependency_outputs: Dict[str, Any], methods: GBPluginMethods):
+        self._utterance_level(dependency_outputs, methods)
+        self._word_level(dependency_outputs, methods)
+        self.successful = True
+
+    def _utterance_level(self):
+        """
+        Prints the entire tree into a CSV file
+        """
+
+        path = os.path.join(self.structure_interact_instance.output_path, OUTPUT_FILE.UTT_CSV)
+
+        with open(path, "w", newline="") as outfile:
+            writer = csv.writer(outfile)
+
+            writer.writerow(CSV_FORMATTER.HEADER)
+
+            l = []
+            l.append(self.structure_interact_instance.apply(self.utterance_level_helper))
+            txt = CSV_FORMATTER.TXT_SEP.join(l)
+
+            sLabel = ""
+                if (
+                    curr_utt[0].sLabel != LABEL.PAUSE
+                    and curr_utt[0].sLabel != LABEL.GAPMARKER
+                ):
+
+    
+    def utterance_level_helper(self, curr):
+        l = []
+        for word in curr:
+            l.append(word.text)
+        txt = CSV_FORMATTER.TXT_SEP.join(l)
+
+        sLabel = ""
+        if (
+            curr_utt[0].sLabel != LABEL.PAUSE
+            and curr_utt[0].sLabel != LABEL.GAPMARKER
+        ):
+            sLabel = LABEL.SPEAKERLABEL + str(curr_utt[0].sLabel)
+            writer.writerow(
+                [sLabel, txt, curr_utt[0].startTime, curr_utt[-1].endTime]
+            )
+        else:
+            writer.writerow(
+                ["", txt, curr_utt[0].startTime, curr_utt[-1].endTime]
+            )
+        if self.is_speaker_utt(curr.speaker):
+            return None
+        return curr.text
+
+
+    def _word_level(self, dependency_outputs: Dict[str, Any], methods: GBPluginMethods):
+        cm: ConversationModel = dependency_outputs[PLUGIN_NAME.ConvModel]
+        varDict = {
+            MARKER.GAPS: LABEL.GAPMARKER,
+            MARKER.OVERLAPS: LABEL.OVERLAPMARKER,
+            MARKER.OVERLAP_FIRST_START: LABEL.OVERLAPMARKER,
+            MARKER.OVERLAP_FIRST_END: LABEL.OVERLAPMARKER,
+            MARKER.OVERLAP_SECOND_START: LABEL.OVERLAPMARKER,
+            MARKER.OVERLAP_SECOND_END: LABEL.OVERLAPMARKER,
+            MARKER.PAUSES: LABEL.PAUSE,
+        }
+
+        root = cm.getTree(False)
+        newUttMap = dict()
+        myFunction = cm.outer_buildUttMapWithChange(0)
+        myFunction(root, newUttMap, varDict)
+
+        path = os.path.join(methods.output_path, OUTPUT_FILE.WORD_CSV)
+        with open(path, "w", newline="") as outfile:
+            writer = csv.writer(outfile)
+
+            writer.writerow(CSV_FORMATTER.HEADER)
+            utterances = newUttMap
+            for _, (_, nodeList) in enumerate(utterances.items()):
+                curr_utt = cm.getWordFromNode(nodeList)
+
+                for word in curr_utt:
+                    sLabel = curr_utt[0].sLabel
+                    if curr_utt[0].sLabel not in MARKER.INTERNAL_MARKER_SET:
+                        sLabel = LABEL.SPEAKERLABEL + str(curr_utt[0].sLabel)
+                        writer.writerow(
+                            [word.sLabel, word.text, word.startTime, word.endTime]
+                        )
+                    else:
+                        writer.writerow(
+                            [word.sLabel, word.text, word.startTime, word.endTime]
+                        )
